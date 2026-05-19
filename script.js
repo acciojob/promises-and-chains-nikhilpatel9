501acb206fcbbbdb2b1d71584c72a0b321d6152f
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-05-19 11:48:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener("click", (event) => {
+    event.preventDefault(); 
+
+    const name = document.getElementById("name").value.trim();
+    const age = document.getElementById("age").value.trim();
+
+    if (!age || !name) {
+        alert("Please enter valid details.");
+        return;
+    }
+
+    let val = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Number(age) >= 18) {
+                resolve(`Welcome, ${name}. You can vote.`);
+            } else {
+                reject(`Oh sorry ${name}. You aren't old enough.`);
+            }
+        }, 4000);
+    });
+
+    val.then((message) => {
+        alert(message);
+    }).catch((error) => {
+        alert(error);
+    });
+});
